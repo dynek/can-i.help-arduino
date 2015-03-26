a81a6204343c9602282b8c5ee995b19b9bb0794e
dynek <pierrick@bs-network.net>
2015-03-26 21:55:22 +0100

    avoid pushing keys onto github by separating them :-)

---
+++ src/can-i.help.h
@@ -0,0 +1,13 @@
+// Not so pretty but these are things I don't want to share on github :-)
+
+// parse.com API
+#define API_SERVER_HOSTNAME "api.parse.com"
+#define API_SERVER_PORT 80
+#define URL_PATH "/1/classes/<class>"
+#define APPLICATION_ID "0w95nbYtKN8uoFmPA8oMUjLhnzpDYPCZzWZ41A6mg"
+#define REST_API_KEY "hFhJ9C1q7FIBkbH8M4QwhksLAPeykVBMWet9Qqvm0"
+#define USER_ID "y0i344AsYx"
+
+// network stuff
+#define HTTP_USER_AGENT "my user agent/1.0"
+#define BUFFER_SIZE 192 // biggest buffer will be payload and basically it is 181 chars max, let's give it some room for longer messages
